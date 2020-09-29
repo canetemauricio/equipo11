2284aa4ac5a7b8a363346724a3b7a2cc4fd2f229
Mauricio <canetemauricio@hotmail.com>
2020-09-29 19:01:22 -0300

    Agrego estructura Servidor y sitios html

--- app.js
+++ app.js
@@ -9,6 +9,29 @@ app.get('/', function (req,res){
 
 })
 
+app.get('/producto', function (req,res){
+    let file= path.resolve('public/vistas/producto.html')
+    res.sendFile(file)
+
+})
+
+app.get('/carrito', function (req,res){
+    let file= path.resolve('public/vistas/carrito.html')
+    res.sendFile(file)
+
+})
+
+app.get('/registro', function (req,res){
+    let file= path.resolve('public/vistas/registro.html')
+    res.sendFile(file)
+
+})
+
+app.get('/login', function (req,res){
+    let file= path.resolve('public/vistas/login.html')
+    res.sendFile(file)
+})
+
 
 
 
