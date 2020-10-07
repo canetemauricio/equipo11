9440e7ec24ada6b63318c15f5afefb2856f01dd7
guilleadominguez <guilleadominguez@yahoo.com.ar>
2020-10-07 19:44:49 -0300

    creacion carrito html y css

--- app.js
+++ app.js
@@ -4,31 +4,31 @@ const app = express();
 const path = require('path')
 
 app.get('/', function (req,res){
-    let file= path.resolve('public/vistas/index.html')
+    let file= path.resolve('./public/vistas/index.html')
     res.sendFile(file)
 
 })
 
 app.get('/producto', function (req,res){
-    let file= path.resolve('public/vistas/producto.html')
+    let file= path.resolve('./public/vistas/producto.html')
     res.sendFile(file)
 
 })
 
 app.get('/carrito', function (req,res){
-    let file= path.resolve('public/vistas/carrito.html')
+    let file= path.resolve('./public/vistas/carrito.html')
     res.sendFile(file)
 
 })
 
 app.get('/registro', function (req,res){
-    let file= path.resolve('public/vistas/registro.html')
+    let file= path.resolve('./public/vistas/registro.html')
     res.sendFile(file)
 
 })
 
 app.get('/login', function (req,res){
-    let file= path.resolve('public/vistas/login.html')
+    let file= path.resolve('./public/vistas/login.html')
     res.sendFile(file)
 })
 
