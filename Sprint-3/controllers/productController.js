b7e671937403a2bc8beeb008289dc1ddae7132e9
Mauricio <canetemauricio@hotmail.com>
2020-10-29 11:19:24 -0300

    updating

--- Sprint-3/controllers/productController.js
+++ Sprint-3/controllers/productController.js
@@ -1,11 +1,12 @@
+const products = require("../data/products.json");
 module.exports = {
 
     show: function(req,res){
-        res.send('Todos los Productos')
+        res.render('./products/list', {title: "CATÁLOGO - MAG", products: products})
     },
 
     detail: function(req,res){
-        res.render('./products/detail')
+        res.render('./products/detail', {title: "DETALLE - MAG"})
     }
 
 
