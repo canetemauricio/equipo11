5620c488a59bd914ad99fee6ad1065e5af737835
NithHG <heberg128@gmail.com>
2020-10-29 18:50:51 -0300

    Fixed file structure

--- app.js
+++ app.js
@@ -1,39 +1,48 @@
-const express = require("express");
-const app = express();
-const path = require("path");
+var createError = require('http-errors');
+const methodOverride = require('method-override')
+const fs = require('fs')
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-app.get("/", function (req, res) {
-  let file = path.resolve("public/vistas/index.html");
-  res.sendFile(file);
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var heroesRouter = require('./routes/products')
+var authRouter = require('./routes/auth')
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(methodOverride('_method'))
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/products', heroesRouter)
+app.use('/users', usersRouter);
+app.use('/auth', authRouter );  
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-app.get("/producto", function (req, res) {
-  let file = path.resolve("public/vistas/producto.html");
-  res.sendFile(file);
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
-app.get("/carrito", function (req, res) {
-  let file = path.resolve("public/vistas/carrito.html");
-  res.sendFile(file);
-});
-
-app.get("/registro", function (req, res) {
-  let file = path.resolve("public/vistas/registro.html");
-  res.sendFile(file);
-});
-
-app.get("/login", function (req, res) {
-  let file = path.resolve("public/vistas/login.html");
-  res.sendFile(file);
-});
-
-app.get("*", function (req, res) {
-  if (req.url.includes(".")) {
-    let file = path.resolve("public" + req.url);
-    return res.sendFile(file);
-  }
-});
-
-app.listen(3000, () => {
-  console.log("MAG está escuchando en el puerto 3000");
-});
+module.exports = app;
