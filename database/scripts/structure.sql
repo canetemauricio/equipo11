468a94da9d6a5bb6f8663bbf83894f26595f4df5
NithHG <heberg128@gmail.com>
2021-02-04 19:40:24 -0300

    uploaded the RIGHT db

--- database/scripts/structure.sql
+++ database/scripts/structure.sql
@@ -1,22 +1,25 @@
 SET sql_mode = '';
 
+DROP SCHEMA MagDB;
+
 CREATE DATABASE MagDB;
+USE MagDB;
 
 CREATE TABLE profile (
 	id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
     user VARCHAR(24) NOT NULL,
     email VARCHAR(32) NOT NULL,    
-    password VARCHAR(24) NOT NULL,    
+    password VARCHAR(100) NOT NULL,    
     remember_token VARCHAR(100),
-    firstname VARCHAR(24) NOT NULL,
-    lastname VARCHAR(24) NOT NULL,
+    firstname VARCHAR(24),
+    lastname VARCHAR(24),
     gender VARCHAR(20),
     DNI VARCHAR(8),
-    birthday DATE NOT NULL,
+    birthday DATE,
     phone VARCHAR(20),
     type VARCHAR(10) NOT NULL,
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP
     );
     
 CREATE TABLE card (
@@ -41,13 +44,23 @@ CREATE TABLE address (
         profileID INT UNSIGNED,
 		FOREIGN KEY (profileID) REFERENCES profile(id)
         );
+        
+CREATE TABLE categories (
+	id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    category VARCHAR(24) NOT NULL
+	);
     
 CREATE TABLE product (
 	id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(24) NOT NULL,
     image VARCHAR(255) NOT NULL,
     description VARCHAR(255) NOT NULL,
-    price DECIMAL(8,2) NOT NULL
+    price DECIMAL(8,2) NOT NULL,
+    gender VARCHAR(10) NOT NULL,
+    brand VARCHAR(20) NOT NULL,
+    deletedAt TIMESTAMP,
+    categoriesID INT UNSIGNED,
+	FOREIGN KEY (categoriesID) REFERENCES categories(id)
     );
     
 CREATE TABLE cart (
@@ -61,30 +74,10 @@ CREATE TABLE cart (
     
 CREATE TABLE product_cart (
 	id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-	quantity INT NOT NULL,
+	quantity INT UNSIGNED NOT NULL,
 	total DECIMAL(8,2) NOT NULL,
 	productID INT UNSIGNED,
 	cartID INT UNSIGNED,
 	FOREIGN KEY (productID) REFERENCES product(id),
 	FOREIGN KEY (cartID) REFERENCES cart(id)
 	);
-
-CREATE TABLE variant (
-	id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    type VARCHAR(24) NOT NULL,
-    subtype VARCHAR(24) NOT NULL,
-    gender VARCHAR(10) NOT NULL,
-    size VARCHAR(3) NOT NULL,
-    color VARCHAR(16) NOT NULL,
-    brand VARCHAR(32) NOT NULL,
-    usestate VARCHAR(5) NOT NULL,
-    quality INT UNSIGNED NOT NULL
-	);
-
-CREATE TABLE product_variant (
-	id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    productID INT UNSIGNED,
-    variantID INT UNSIGNED,
-    FOREIGN KEY (productID) REFERENCES product(id),
-    FOREIGN KEY (variantID) REFERENCES variant(id)
-    );    
